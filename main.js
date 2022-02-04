fb03c590034628a5c758e32a2d55339f232b6df0
Matteo Morganti <matteo99morganti@gmail.com>
2022-02-04 15:50:13 +0100

    creato funzione colore casuale

--- main.js
+++ main.js
@@ -156,7 +156,7 @@ function drawIcons(container, icons){
     // scorro nel array e creo le icone nella pagina
     icons.forEach(oggetto =>{
         content += `<div class="icon">
-                        <i style="color:${oggetto.color};" class="${oggetto.family} ${oggetto.prefix}${oggetto.name}"></i>
+                        <i class="color-${oggetto.color} ${oggetto.family} ${oggetto.prefix}${oggetto.name}"></i>
                         <div class="icon-text">${oggetto.name}</div>
                     </div> 
                      `;
@@ -165,3 +165,19 @@ function drawIcons(container, icons){
     container.innerHTML = content;
 
 }
+
+
+
+
+// creo una funzione che mi generi in modo casuale un codice colore, formato da # iniziale
+// e seguito da 6 caratteri alfanumerici compresi tra 0 e 9 e A e F
+function RandomColore(length) {
+    let result = '#';
+    let characters = 'abcdef0123456789';
+    for ( let i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * characters.length));
+   }
+   return result;
+}
+
+console.log(RandomColore(6));
